d76dd5e32563c239d30783985e7cdae948a8d2f5
mufaddal <mufaddalaurangabadi@gmail.com>
2024-05-20 11:22:49 +0530

    main banner

--- components/Common/PageBanner.jsx
+++ components/Common/PageBanner.jsx
@@ -7,13 +7,11 @@ const PageBanner = ({ headline, subHeadline, image }) => {
   const sectionRef = useRef(null);
   const isInView = useInView(sectionRef, { once: false });
   return (
-    <main
-      className="bg-gray-50 items-center bg-cover bg-center bg-no-repeat"
-      style={{ backgroundImage: "url('./heroBg.png')" }}>
-      <section className="container py-[100px] flex items-center">
+    <main className="bg-gray-50 items-center bg-cover bg-center bg-no-repeat">
+      <section className="flex items-center">
         <article className="flex flex-col lg:flex-row justify-between items-center w-full">
           <m.div
-            className="w-full lg:w-7/12"
+            className="w-full lg:w-6/12 flex flex-col items-center justify-center"
             ref={sectionRef}
             initial={{ x: -20, opacity: 0 }}
             animate={isInView ? { x: 0, opacity: 1 } : {}}
@@ -24,19 +22,22 @@ const PageBanner = ({ headline, subHeadline, image }) => {
             <p className="text-gray-600 text-sm lg:text-lg">{subHeadline}</p>
           </m.div>
           <m.div
-            className="w-full lg:w-5/12 p-2 flex mb-4 justify-center items-center order-first lg:order-last"
+            className="w-full h-[550px] lg:w-6/12 p-2 flex justify-center items-center order-first lg:order-last bg-no-repeat bg-center bg-cover"
             ref={sectionRef}
             initial={{ x: 20, opacity: 0 }}
             animate={isInView ? { x: 0, opacity: 1 } : {}}
-            transition={{ ease: "easeInOut", duration: 0.75 }}>
-            <Image
-              className="rounded-xl"
-              src={image}
-              width={800}
-              height={500}
-              alt="Banner image"
-              priority // Helps to preload the image for faster loading
-            />
+            transition={{ ease: "easeInOut", duration: 0.75 }}
+            style={{ backgroundImage: `url(${image})` }}>
+            {/* 
+  <Image
+    className="rounded-xl"
+    src={image}
+    width={800}
+    height={500}
+    alt="Banner image"
+    priority // Helps to preload the image for faster loading
+  /> 
+  */}
           </m.div>
         </article>
       </section>
